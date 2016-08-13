a34a96a7191000b41f413ae0d353864fe76abda2
Will McAuliff <womcauliff@gmail.com>
2016-08-13 00:06:46 -0400

    Bind onclick handler to form submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
 $(document).ready(function () {
 	console.log("ready()");
 });
+
+
+var trainName = "";
+var destination = "";
+var firstTime = "";
+var frequency = "";
+$("#formsubmit").on("click", function() {
+	trainName = $("#train-name").val().trim();
+	destination = $("#destination").val().trim();
+	firstTime = $("#first-time").val().trim();
+	frequency = $("#frequency").val().trim();
+
+	if (
+		trainName === "" 
+		|| destination === "" 
+		|| firstTime === "" 
+		|| frequency === ""
+	) {
+		console.log("empty form, return");
+		return false;
+	}
+
+	console.log(trainName);
+	console.log(destination);
+	console.log(firstTime);
+	console.log(frequency);
+
+	return false;
+});
