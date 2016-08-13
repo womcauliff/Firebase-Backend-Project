c06efb8a693ba02c73c0d213209efb7f811b6b3a
Will McAuliff <womcauliff@gmail.com>
2016-08-13 01:08:54 -0400

    Form submit pushes to firebase. Frontend displays firebase entries.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,17 @@ $(document).ready(function () {
 	console.log("ready()");
 });
 
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyDfFOZ_JxVo5PNHmhYG99zQX1JOyrArzP0",
+authDomain: "fir-backend-project.firebaseapp.com",
+databaseURL: "https://fir-backend-project.firebaseio.com",
+storageBucket: "fir-backend-project.appspot.com",
+};
+firebase.initializeApp(config);
 
+var database = firebase.database();
+    
 var trainName = "";
 var destination = "";
 var firstTime = "";
@@ -20,7 +30,7 @@ $("#formsubmit").on("click", function() {
 		|| frequency === ""
 	) {
 		console.log("empty form, return");
-		return false;
+		return false;//prevent page refresh
 	}
 
 	console.log(trainName);
@@ -28,5 +38,34 @@ $("#formsubmit").on("click", function() {
 	console.log(firstTime);
 	console.log(frequency);
 
-	return false;
+	database.ref().push({
+		trainName: trainName,
+		destination: destination,
+		firstTime: firstTime,
+		frequency: frequency,
+		dateAdded: firebase.database.ServerValue.TIMESTAMP
+	});
+
+	return false;//prevent page refresh
 });
+
+database.ref().on("child_added", function(childSnapshot) {
+
+	if(childSnapshot.val() == null) {
+		return;
+	}
+
+	console.log(childSnapshot.val());
+	
+	var trainEntry = $("<div>").addClass("col-md-12");
+
+	trainEntry.append($("<div>").addClass("col-md-5ths").text(childSnapshot.val().trainName));
+	trainEntry.append($("<div>").addClass("col-md-5ths").text(childSnapshot.val().destination));
+	trainEntry.append($("<div>").addClass("col-md-5ths").text(childSnapshot.val().firstTime));
+	trainEntry.append($("<div>").addClass("col-md-5ths").text(childSnapshot.val().frequency));
+	trainEntry.append($("<div>").addClass("col-md-5ths").text(childSnapshot.val().dateAdded));
+
+	$("#trains").append($("<div class='row trainrow'>").append(trainEntry));
+}, function(errorObject) {
+	console.log("Errors handled: " + errorObject.code);
+})
